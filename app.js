2c9564f713c94c6aedd1f2e0e073269f99eb789d
stephaniee2 <stephaniejlouie@gmail.com>
2018-09-12 15:44:02 -0500

    updated my cruddy code. Still in progress.

--- app.js
+++ app.js
@@ -1,61 +1,78 @@
 $(document).ready(function() {
+    
+    
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('item');
+    
 
+  $(".add-ingred-btn").on("click", function(event){
 
-  $(".add-text-btn").on("click", function(){
-
-    // store values
-    let inputIngredient = $(".ingredient").val();
-    let inputCups = $(".cups").val();
-
-    // clear values
-    $(".ingredient").val("");
-    $(".cups").val("");
-
-    console.log(inputIngredient, inputCups);
-
+      // store values
+      let inputIngredient = $(".ingredient").val();
+      let inputCups = $(".cups").val();
+      
     localStorage.setItem(inputIngredient, inputCups);
     // data-
-    let itemHtml = '<div class="display-item" data-storage-key="'
-    +inputIngredient+'"> ' + inputCups + ' ' 
-    + localStorage.getItem(inputIngredient) + '</div>';
-    $(".display").html(itemHtml);
+    // let itemHtml = '<div class="display-item" data-storage-key="'
+    // +inputIngredient+'"> ' + inputCups + ' ' 
+    // + localStorage.getItem(inputIngredient) + '</div>';
+    // $(".display").html(itemHtml);
     //console.log(localStorage);
     // how can we delegate this event to the outer html node?
     // https://learn.jquery.com/events/event-delegation/
 
-    $(".add-text-btn").on("click", function(e){
-      // plop the key:value back into the input boxes
-      data.push([inputIngredient, inputCups]);
-      chart.load()
-
-      // get the values from the the divs?
-      console.log("key=> ", e.target.dataset.storageKey); // user-input-title
-      localStorage.getItem(e.target.dataset.storageKey); // user-input-body
-
-      // set those values in the form fields
-      $(".ingredient").val(e.target.dataset.storageKey);
-      $(".cups").val(localStorage.getItem(e.target.dataset.storageKey));
+    chart.load({
+      columns:[
+        [inputIngredient, inputCups]
+      ]
     });
+    console.log('user input ', inputIngredient, inputCups);
 
+        // clear values
+        $(".ingredient").val("");
+        $(".cups").val("");
   });
+    
 
+//  function newElement() {
+//    var value = $('.direction').val();
+//    var li = document.createElement("li");
+//    // var inputValue = document.getElementsByClassName("direction").value;
+//    var t = document.createTextNode(value);
+//    li.appendChild(t);
+//    if (value === '') {
+//      alert("You must write something!");
+//    } else {
+//      document.getElementById("ordered-steps").appendChild(li);
+//    }
+//    // document.getElementsById("ordered-steps").value = "";
+//  
+//    var span = document.createElement("SPAN");
+//    var txt = document.createTextNode("\u00D7");
+//    span.className = "close";
+//    span.appendChild(txt);
+//    li.appendChild(span);
+//  
+//    for (i = 0; i < close.length; i++) {
+//      close[i].onclick = function() {
+//        var div = this.parentElement;
+//        div.style.display = "none";
+//      }
+//    }
+//  }
+  
 
-
-   // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
+//  $(".add-direction-btn").on("click", function(event){
+//    return newElement()
+//  })
 
    $(".del-text-btn").on("click", function() {
      alert('item deleted? check the console'); // maybe change to a window.confirm
      localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
      $(".ingredient").val("");
      $(".user-input-body").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
    });
 
 
---
+++ chart.js
@@ -0,0 +1,109 @@
+// Your code here!
+var chart = c3.generate({
+    data: {
+        // iris data from R
+        columns: [
+            ['input your first ingredient', 1],
+            // ['ingred2', 120],
+            // ['ingred3', 100],
+
+        ],
+
+        type : 'donut', 
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d); },
+        onmouseout: function (d, i) { console.log("onmouseout", d); },
+        colors: {
+        'sleep': 'pink',
+        'eat': 'dodgerblue',
+        'code': 'violet',
+        'relax': 'grey',
+        'other': 'aquamarine'
+        },
+        color: function (color, d) {
+            // d will be 'id' when called for legends
+            return d.id && d.id === 'data3' ? d3.rgb(color).darker(d.value / 150) : color;
+        }
+    },
+    pie: {
+        label: {
+            format: function (value) {
+                return d3.format(value)(' cups');
+            }
+        }
+    }
+});
+
+
+
+var chart = c3.generate({
+    data: {
+        // iris data from R
+        columns: [
+            ['input your first ingredient', 1],
+            // ['ingred2', 120],
+            // ['ingred3', 100],
+
+        ],
+
+        type : 'donut', 
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d); },
+        onmouseout: function (d, i) { console.log("onmouseout", d); },
+        colors: {
+        'sleep': 'pink',
+        'eat': 'dodgerblue',
+        'code': 'violet',
+        'relax': 'grey',
+        'other': 'aquamarine'
+        },
+        color: function (color, d) {
+            // d will be 'id' when called for legends
+            return d.id && d.id === 'data3' ? d3.rgb(color).darker(d.value / 150) : color;
+        }
+    },
+    pie: {
+        label: {
+            format: function (value) {
+                return d3.format(value)(' cups');
+            }
+        }
+    }
+});
+
+// setTimeout(function () {
+//     chart.load(
+//         {
+//         columns: [
+//             ["sleep", 8],
+//             ["eat", 3],
+//             ["code", 7],
+//             ["relax", 2],
+//             ["other", 4],
+//         ]
+//     }
+    
+//     );
+// }, 1500);
+
+// chart.load(
+//     {
+//     columns: [
+//         [data, 8],
+//         ["eat", 3],
+//         ["code", 7],
+//         ["relax", 2],
+//         ["other", 4],
+//     ]
+// }
+
+// );
+
+setTimeout(function () {
+    chart.unload({
+        ids: 'data1'
+    });
+    chart.unload({
+        ids: 'data2'
+    });
+}, 1500);
