28caaadf57eea247a84d73dbf50b3103b1445551
stephaniee2 <stephaniejlouie@gmail.com>
2018-09-13 01:09:55 -0500

    almost working code for my cruddy to-do app

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 $(document).ready(function() {
     
-// store values
-let inputTask = $("#tasks").val();
-let inputHours = $("#hours").val();
-    
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 const input = document.getElementById('tasks');
 
 let taskArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
-    
-localStorage.setItem('tasks', JSON.stringify(taskArray));
-const data = JSON.parse(localStorage.getItem('tasks'));    
+let deleteArray = localStorage.getItem('deleteTasks') ? JSON.parse(localStorage.getItem('deleteTasks')) : [];
+//    
+//let taskChart = function(){
+//    let taskAndHoursHolder = [];
+//    if(localStorage.getItem('hours') === undefined){
+//        chart.load({
+//        columns:[
+//        [inputTask, inputHours]
+//      ]
+//    })
+//} else {
+//        for(var aTask of (localStorage.getItem('tasks'))){
+//            taskAndHoursHolder.push([aTask]);
+//        };
+//        for(var someHours of (localStorage.getItem('hours'))){
+//            taskAndHoursHolder.forEach(function(item){
+//                item.push(someHours);
+//            })
+//        }
+//    chart.load({
+//        columns:taskAndHoursHolder
+//        })
+//    }
+//};
+     
+////update chart
+//let taskChart;
+//if (localStorage.getItem('tasks')) {
+//  localStorage.forEach(function(item){
+//      chart.load({
+//      columns:[
+//        [key, value]
+//      ]
+//    });
+//  });
+//}
+
+const taskData = JSON.parse(localStorage.getItem('tasks'));        
 
 //function that creates a list element
 const addToList = function(text){ 
@@ -21,68 +52,52 @@ const addToList = function(text){
       ul.appendChild(li);
     };
 
-
-    
-   
- 
     
   $(".add-task-btn").on("click", function(event){
       let inputTask = $("#tasks").val();
-      let inputHours = $("#hours").val();
-      
+      let inputDeleteTask = $("#deleteTasks").val();
+      console.log(taskData)
     event.preventDefault();  
       
-    //adding task to localstorage  
+    //adding input data to to do list
     taskArray.push(inputTask);
     localStorage.setItem('tasks', JSON.stringify(taskArray));
     addToList(inputTask);
       
+    //adding input data to chart (ORIGINAL)
+//    chart.load({
+//      columns:[
+//        [inputTask, inputHours],
+//          
+//      ]
+//    });
       
-    // data-
-    // let itemHtml = '<div class="display-item" data-storage-key="'
-    // +inputIngredient+'"> ' + inputCups + ' ' 
-    // + localStorage.getItem(inputIngredient) + '</div>';
-    // $(".display").html(itemHtml);
-    //console.log(localStorage);
-    // how can we delegate this event to the outer html node?
-    // https://learn.jquery.com/events/event-delegation/
-
-    chart.load({
-      columns:[
-        [inputTask, inputHours]
-      ]
-    });
+       ////adding input data to chart (USING HELPER FUNCTION)
+//      taskChart();
       
 //    console.log('user input ', inputTask, inputHours);
 
         // clear values
     $("#tasks").val("");
-    $("#hours").val("");
-      
+    $("#deleteTasks").val("");
  
   });
     
-   
-
-    
-    data.forEach(function(item){
+    taskData.forEach(function(item){
         addToList(item);
     });
     
     
-   $(".del-text-btn").on("click", function() {
-     alert('item deleted? check the console'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
-     $("#tasks").val("");
-     $(".user-input-body").val("");
-   });
-
-
-   // iterative approach to adding items
-   // store data as stringified array of objects
-   // store data with individual keys
-  // how do we get keys? research Object.keys
-
-
-
+//   $(".del-task-btn").on("click", function() {
+//     alert('are you sure you want to start a new list? Your data will not be saved.'); 
+//       console.log(localStorage.tasks);
+//     let filteredArr = (JSON.parse(localStorage.tasks)).filter(item => item !== $("#deleteTasks").val());
+//     localStorage.tasks = filteredArr;
+////     localStorage.removeItem($("#deleteTasks").val()); 
+//     if(ul === ($("#deleteTasks").val())){
+//         ul.remove();
+//     }
+//     $("#deleteTasks").val("");
+//     $("#tasks").val("");
+//   });
 });
